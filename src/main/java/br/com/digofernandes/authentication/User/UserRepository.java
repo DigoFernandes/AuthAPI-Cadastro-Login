82dcfd13396e3204068b4b1eab13006357795ea6
DigoFernandes <digofernandes017@gmail.com>
2024-03-29 23:33:18 -0300

    user repository

--- src/main/java/br/com/digofernandes/authentication/User/UserRepository.java
+++ src/main/java/br/com/digofernandes/authentication/User/UserRepository.java
@@ -1,5 +1,8 @@
 package br.com.digofernandes.authentication.User;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
     
-}
+} 
